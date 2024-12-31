a71ba8af65d7b72ea41525703f183e0ef3955e0f
Tony Hewett <18242203+tonyhewett@users.noreply.github.com>
2024-12-31 03:40:18 +0000

    https://www.youtube.com/watch?v=-8OEfGQPJ8c

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ require("./database/database.js").connect()
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req,res) => {
     res.send("<h1>server is working today .... </h1>")
+})
+
+app.post("/register", async (req,res) => {
+    try {
+        // get all data from body
+        const {firstName, lastName, email, password} = req.body
+        // all the data should exist
+        if (!(firstName && lastName && email && password)) {
+            res.status(400)
+        }
+        // check if user already exists
+        // encrypt the password
+        // save the user in db
+        // generate a token for user and send it
+        
+    } catch (error) {
+        console.log(error();
+        );
+        
+    }
 })
 
 module.exports = app
