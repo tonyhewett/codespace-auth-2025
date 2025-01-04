5d9dedd47aa4b0e9e172e8efe5a787b9f8b9ff0e
Tony Hewett <18242203+tonyhewett@users.noreply.github.com>
2025-01-04 22:25:59 +0000

    minor note changes, assignment of url change

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
-const MONGODB_URL = process.env.MONGODB_URL
-//console.log(MONGODB_URL);
+// const MONGODB_URL = process.env.MONGODB_URL
+const { MONGODB_URL } = process.env 
+console.log(MONGODB_URL);
 //console.log(typeof MONGODB_URL); 
 
 exports.connect = () => {
@@ -9,12 +10,12 @@ exports.connect = () => {
         //useNewUrlParser: true,
         //useUnifiedTopology: true
     })
-    .then(
-        console.log('database connected')
+    .then (
+        console.log('database connected')    
     )
     .catch((error) => {
         console.log('db connection failed');
         console.log(error);
-        process.exit(1);        
+        process.exit(1);        // code 1 means somthing went wrong
     })
 }
